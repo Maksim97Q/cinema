a1f05aa4463cfe77db5f868c648c3284daf785ca
Максим Чикилёв <flion9@mail.ru>
2021-12-17 20:48:58 +0300

    2

--- It-Academy-Task/web/src/main/webapp/WEB-INF/servlet/HelloServlet.java
+++ It-Academy-Task/web/src/main/webapp/WEB-INF/servlet/HelloServlet.java
@@ -9,5 +9,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.getWriter().write("Hello");
+        resp.getWriter().write("Hello");
+
     }
 }
